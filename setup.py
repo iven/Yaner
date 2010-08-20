c216a808dc29cb3e2bffc2348840a8a44de11bac
iven <ivenvd@gmail.com>
2010-08-20 23:50:56 +0800

    Rewrited setup.py.

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
-import sys, os, glob
-import subprocess
+import sys, os
+from glob import glob
+from os.path import basename, splitext, join, isdir
 from stat import *
 from distutils.core import setup
-from distutils.command.install import install as _install
-from distutils.command.install_data import install_data as _install_data
+from distutils.command.install import install
+from distutils.command.install_data import install_data
 
 INSTALLED_FILES = "installed_files"
 
-class install(_install):
+class Install(install):
 
     def run(self):
-        _install.run(self)
+        install.run(self)
         outputs = self.get_outputs()
         length = 0
         if self.root:
@@ -22,52 +23,39 @@ class install(_install):
         if length:
             for counter in xrange(len(outputs)):
                 outputs[counter] = outputs[counter][length:]
-        data = "\n".join(outputs)
-        try:
-            file = open(INSTALLED_FILES, "w")
-        except:
-            self.warn("Could not write installed files list %s" % \
-                       INSTALLED_FILES)
-            return 
-        file.write(data)
-        file.close()
+        with open(INSTALLED_FILES, "w") as install_file:
+            install_file.write("\n".join(outputs))
 
-class install_data(_install_data):
+class InstallData(install_data):
 
     def run(self):
-        def chmod_data_file(file):
+        def chmod_data_file(data_file):
             try:
-                os.chmod(file, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+                os.chmod(data_file, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
             except:
                 self.warn("Could not chmod data file %s" % file)
-        _install_data.run(self)
+        install_data.run(self)
         map(chmod_data_file, self.get_outputs())
 
-class uninstall(_install):
+class Uninstall(install):
 
     def run(self):
-        try:
-            file = open(INSTALLED_FILES, "r")
-        except:
-            self.warn("Could not read installed files list %s" % \
-                       INSTALLED_FILES)
-            return 
-        files = file.readlines()
-        file.close()
+        with open(INSTALLED_FILES, "r") as install_file:
+            install_files = install_file.readlines()
         prepend = ""
         if self.root:
             prepend += self.root
         if self.prefix:
             prepend += self.prefix
         if len(prepend):
-            for counter in xrange(len(files)):
-                files[counter] = prepend + files[counter].rstrip()
-        for file in files:
-            print "Uninstalling %s" % file
+            for counter in xrange(len(install_files)):
+                install_files[counter] = prepend + install_files[counter].rstrip()
+        for install_file in install_files:
+            print "Uninstalling %s" % install_file
             try:
-                os.unlink(file)
+                os.unlink(install_file)
             except:
-                self.warn("Could not remove file %s" % file)
+                self.warn("Could not remove file %s" % install_file)
 
 ops = ("install", "build", "sdist", "uninstall", "clean")
 
@@ -98,71 +86,30 @@ if sys.argv[1] in ("install", "uninstall") and len(prefix):
 
 with open("VERSION") as version_file:
     version = version_file.read().strip()
-    if "=" in version:
-        version = version.split("=")[1]
 
-with open(os.path.join("Yaner/Constants.py.in")) as f:
+with open(join("Yaner/Constants.py.in")) as f:
     data = f.read()
 
 data = data.replace("@prefix@", prefix)
 data = data.replace("@version@", version)
 
-with open(os.path.join("Yaner/Constants.py"), "w") as f:
+with open(join("Yaner/Constants.py"), "w") as f:
     f.write(data)
 
+po_buildcmd = "msgfmt -o build/locale/%s/yaner.mo po/%s.po"
+for po_file in glob('po/*.po'):
+    po_name = splitext(basename(po_file))[0]
+    if sys.argv[1] in ('build', 'install'):
+        if not isdir("build/locale/" + po_name):
+            os.makedirs("build/locale/" + po_name)
+        os.system(po_buildcmd % (po_name, po_name))
+
 data_files = []
-
-global_icon_path = "share/icons/hicolor/"
-local_icon_path = "share/yaner/icons/hicolor/"
-
-for dir, subdirs, files in os.walk("images/"):
-    global_images = []
-    images = []
-    for file in files:
-        if file.endswith(".png") or file.endswith(".svg"):
-            file_path = "/".join((dir, file))
-            if file[:-4] == "yaner":
-                global_images.append(file_path)
-            else:
-                images.append(file_path)
-    if len(global_images) > 0:
-        data_files.append((global_icon_path + dir[7:], global_images))
-    if len(images) > 0:
-        data_files.append((local_icon_path + dir[7:], images))
-
-podir = os.path.realpath("./po")
-if os.path.isdir (podir):
-    buildcmd = "msgfmt -o build/locale/%s/yaner.mo po/%s.po"
-    mopath = "build/locale/%s/yaner.mo"
-    destpath = "share/locale/%s/LC_MESSAGES"
-    for name in os.listdir (podir):
-        if name[-2:] == "po":
-            name = name[:-3]
-            if sys.argv[1] == "build" \
-               or (sys.argv[1] == "install" and \
-                   not os.path.exists (mopath % name)):
-                if not os.path.isdir ("build/locale/" + name):
-                    os.makedirs ("build/locale/" + name)
-                os.system (buildcmd % (name, name))
-            data_files.append ((destpath % name, [mopath % name]))
-
-gladedir = os.path.realpath("./glade")
-if os.path.isdir (gladedir):
-    glade_files = []
-    destpath = "share/yaner/glade/"
-    for file in os.listdir (gladedir):
-        glade_files.append("/".join((gladedir, file)))
-    data_files.append((destpath, glade_files))
-
-configdir = os.path.realpath("./config")
-if os.path.isdir (configdir):
-    config_files = []
-    destpath = "share/yaner/config/"
-    for file in os.listdir (configdir):
-        config_files.append("/".join((configdir, file)))
-    data_files.append((destpath, config_files))
-
-data_files.append(('share/applications/', [os.path.realpath('./yaner.desktop')]))
+data_files.append(("share/locale/%s/LC_MESSAGES" % po_name,
+    glob('build/locale/%s/yaner.mo' % po_name)))
+data_files.append(("share/yaner/glade/", glob('glade/*')))
+data_files.append(("share/yaner/config/", glob('config/*')))
+data_files.append(('share/applications/', ['yaner.desktop']))
 
 setup (
         name             = "yaner",
@@ -175,19 +122,9 @@ setup (
         data_files       = data_files,
         packages         = ["Yaner"],
         scripts          = ["yaner"],
-        cmdclass         = {"uninstall" : uninstall,
-                            "install" : install,
-                            "install_data" : install_data}
+        cmdclass         = {"uninstall" : Uninstall,
+                            "install" : Install,
+                            "install_data" : InstallData}
      )
 
 os.remove ("Yaner/Constants.py")
-
-#if sys.argv[1] == "install":
-#    gtk_update_icon_cache = "gtk-update-icon-cache -f -t %s/share/{,yaner/}icons/hicolor" % prefix
-#    root_specified = len([s for s in sys.argv if s.startswith("--root")]) > 0
-#    if not root_specified:
-#        print "Updating Gtk+ icon cache."
-#        os.system(gtk_update_icon_cache)
-#    else:
-#        print """*** Icon cache not updated. After install, run this:
-#***     %s""" % gtk_update_icon_cache
