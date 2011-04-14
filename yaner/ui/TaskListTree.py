2831735cd2297ab694db8bf0cb42772c78f3cb42
iven <ivenvd@gmail.com>
2011-04-14 19:35:57 +0800

    Added TaskListView class, WIP.

--- yaner/ui/TaskListTree.py
+++ yaner/ui/TaskListTree.py
@@ -147,7 +147,7 @@ class TaskListModel(gtk.TreeStore, LoggingMixin):
                 self.columns.STATUS, task.status,
                 self.columns.NAME, task.name,
                 self.columns.PRGRESS_VALUE, task.progress_value,
-                self.columns.PRGRESS_VALUE, task.progress_text,
+                self.columns.PRGRESS_TEXT, task.progress_text,
                 self.columns.SIZE, task.size,
                 self.columns.DOWNLOAD_SPEED, task.download_speed,
                 self.columns.UPLOAD_SPEED, task.upload_speed,
@@ -167,4 +167,89 @@ class TaskListModel(gtk.TreeStore, LoggingMixin):
                 return iter_
             iter_ = self.iter_next(iter_)
         return None
+
+class TaskListView(gtk.TreeView):
+    """
+    The C{gtk.TreeView} displaying L{TaskListModel}.
+    """
+
+    def __init__(self, model):
+        """
+        L{TaskListView} initializing.
+        @arg model:The interface of the tree view.
+        @type model:L{TaskListModel}
+        """
+        gtk.TreeView.__init__(self, model)
+
+        self._model = model
+
+        # Set up TreeViewColumn
+        column = gtk.TreeViewColumn()
+        self.append_column(column)
+
+        renderer = gtk.CellRendererPixbuf()
+        column.pack_start(renderer, False)
+        column.set_cell_data_func(renderer, self._pixbuf_data_func)
+
+        renderer = gtk.CellRendererText()
+        column.pack_start(renderer, True)
+        column.set_cell_data_func(renderer, self._markup_data_func)
+
+        # TreeView properties
+        self.set_headers_visible(False)
+        self.set_show_expanders(False)
+        self.set_level_indentation(16)
+        self.expand_all()
+
+        self.selection.set_mode(gtk.SELECTION_SINGLE)
+
+    @property
+    def model(self):
+        """Get the L{model<TaskListModel>} of the tree view."""
+        return self._model
+
+    @property
+    def selection(self):
+        """Get the C{gtk.TreeSelection} of the tree view."""
+        return self.get_selection()
+
+    def _pixbuf_data_func(self, cell_layout, renderer, model, iter_):
+        """Method for set the icon and its size in the column."""
+        stock_id = model.get_value(iter_, self.model.columns.STATUS)
+        renderer.set_properties(
+                stock_id = stock_id,
+                stock_size = gtk.ICON_SIZE_LARGE_TOOLBAR,
+                )
+
+    def _markup_data_func(self, cell_layout, renderer, model, iter_):
+        """
+        Method for format the text in the column.
+        """
+        (name, description) = model.get(
+                iter_,
+                self.model.columns.NAME,
+                self.model.columns.DOWNLOAD_SPEED,
+                )
+        # Get current state of the iter
+        if self.selection.iter_is_selected(iter_):
+            if self.has_focus():
+                state = gtk.STATE_SELECTED
+            else:
+                state = gtk.STATE_ACTIVE
+        else:
+            state = gtk.STATE_NORMAL
+        # Get the color for the description
+        color = get_mix_color(self, state)
+
+        markup = '<small>' \
+                     '<b>{name}</b>\n' \
+                     '<span fgcolor="{color}">{description}</span>' \
+                 '</small>' \
+                 .format(**locals())
+
+        renderer.set_properties(
+                markup = markup,
+                ellipsize_set = True,
+                ellipsize = pango.ELLIPSIZE_MIDDLE,
+                )
 
