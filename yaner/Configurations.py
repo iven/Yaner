3019bcfd749c50471591437f85bd48a263365fe4
iven <ivenvd@gmail.com>
2011-11-13 17:12:38 +0800

    Removed unnecessary configs.

--- yaner/Configurations.py
+++ yaner/Configurations.py
@@ -24,100 +24,15 @@
 This module contains the default configuration dicts of L{yaner}.
 """
 
-import os
-
 GLOBAL_CONFIG = {
-        'info': {
-            'pools': ['ea1ab4a7-6973-4b7b-8cb0-09e11ab42a14'],
-            },
         'global': {
             'max-overall-download-limit': 0,
             'max-overall-upload-limit': 0,
             },
-        'task': {
-            'dir': '',
-            'out': '',
-            'split': 5,
-            'referer': '',
-            'all-proxy': '',
-            'auto-file-renaming': 'true',
-
-            'http-user': '',
-            'http-passwd': '',
-            'ftp-user': '',
-            'ftp-passwd': '',
-            'http-proxy-user': '',
-            'http-proxy-passwd': '',
-            'ftp-proxy-user': '',
-            'ftp-proxy-passwd': '',
-
-            'connect-timeout': 60,
-            'timeout': 60,
-            'max-tries': 5,
-            'max-file-not-found': 5,
-            'max-connection-per-server': 3,
-
-            'follow-torrent': 'false',
-            'follow-metalink': 'false',
-            'seed-time': 120,
-            'seed-ratio': 2.00,
-            'bt-max-open-files': 100,
-            'bt-max-peers': 55,
-            'bt-tracker-connect-timeout': 60,
-            'bt-tracker-timeout': 60,
-            'bt-prioritize-piece': ['head', 'tail'],
-
-            'metalink-servers': 5,
-            'metalink-language': '',
-            'metalink-location': '',
-            'metalink-os': '',
-            'metalink-version': '',
-            }
         }
 """The global configuration dict of the application."""
 
-POOL_CONFIG = {
-        'info': {
-            'name': _("My Computer"),
-            'host': 'localhost',
-            'port': 6800,
-            'user': '',
-            'passwd': '',
-            'session_id': '',
-            'queuing': '',
-            'categories': [''],
-            'dustbin': '',
-            },
-        }
-"""The pool configuration dict of the application."""
-
-QUEUING_CONFIG = {
-        'info': {
-            'tasks': [],
-            },
-        }
-"""The queuing configuration dict of the application."""
-
-CATEGORY_CONFIG = {
-        'info': {
-            'name': _("Default Category"),
-            'dir': os.path.expanduser('~'),
-            'tasks': [],
-            },
-        }
-"""The category configuration dict of the application."""
-
-DUSTBIN_CONFIG = {
-        'info': {
-            'tasks': [],
-            },
-        }
-"""The dustbin configuration dict of the application."""
-
 TASK_CONFIG = {
-        'info': {
-            'size': 0,
-            },
         'options': {
             'all-proxy': '',
             'http-proxy-user': '',
